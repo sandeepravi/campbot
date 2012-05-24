14a54d7f0ff6f6798f88cbd48aa0f20e29c5412f
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-05-24 19:35:40 +0530

    Added missed out files

--- campbot.gemspec
+++ campbot.gemspec
@@ -14,4 +14,6 @@ Gem::Specification.new do |gem|
   gem.name          = "campbot"
   gem.require_paths = ["lib"]
   gem.version       = Campbot::VERSION
+  gem.add_development_dependency "rspec"
+  gem.add_dependency "tinder"
 end
--- lib/campbot.rb
+++ lib/campbot.rb
@@ -1,5 +1,11 @@
 require "campbot/version"
 
-module Campbot
-  # Your code goes here...
+class Campbot
+
+  def initialize(config)
+
+    raise ArgumentError, "You must pass an API key" unless options[:api_key]
+    raise ArgumentError, "You must pass a subdomain" unless options[:subdomain]
+
+  end
 end
