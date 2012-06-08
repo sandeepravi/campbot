fdcb05b72525499eeed06b9d7efd815ab07327b5
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-06-08 12:06:56 +0530

    Added documentation

--- lib/campbot/message.rb
+++ lib/campbot/message.rb
@@ -8,6 +8,11 @@ class Campbot
   #   # => Campbot
   module Message
 
+    # Public: Parse the message that you get from campfire API
+    #
+    # params - message - A String containing the message that needs to be parsed
+    #
+    # Returns nothing and calls the code that needs to be executed
     def parse(message)
       return unless message["body"].is_a?(String)
       # Condition to check that bot does not get confused and starts 'listening to itself'
