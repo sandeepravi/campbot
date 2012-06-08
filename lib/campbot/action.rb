e1bfba828b45982be00471586a01196307eeb73c
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-06-08 12:06:56 +0530

    Added documentation

--- lib/campbot/action.rb
+++ lib/campbot/action.rb
@@ -8,6 +8,12 @@ class Campbot
   #   # => Campbot
   module Action
 
+    # Public: Method used to post a message to campfire using the API
+    #
+    # params - message - The message that needs to be posted
+    #          type - The type of message, default to 'TextMessage'
+    #
+    # Returns the Object after posting the messing to campfire
     def say(message, type = 'TextMessage')
       request = Net::HTTP::Post.new("/room/#{@room}/speak.json", 'Content-Type' => 'application/json')
       request.body = "{\"message\":{\"type\":\"#{type}\",\"body\":\"#{message}\"}}"
